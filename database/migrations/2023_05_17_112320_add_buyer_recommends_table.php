3a253e761c626153eb9ef5e63fe7f6868d7c0062
arcion-git <info@arcion.jp>
2023-06-12 00:52:36 +0900

    save

--- database/migrations/2023_05_17_112320_add_buyer_recommends_table.php
+++ database/migrations/2023_05_17_112320_add_buyer_recommends_table.php
@@ -14,12 +14,12 @@ class AddBuyerRecommendsTable extends Migration
      public function up()
      {
          Schema::table('buyer_recommends', function (Blueprint $table) {
-             $table->string('groupe')->nullable()->comment('グループ');
-             $table->string('hidden_price')->nullable()->comment('価格非表示');
-             $table->boolean('zaikokanri')->nullable()->comment('在庫管理');
-             $table->string('zaikosuu')->nullable()->comment('在庫数');
-             $table->string('uwagaki_item_name')->nullable()->comment('上書き商品名');
-             $table->string('uwagaki_kikaku')->nullable()->comment('上書き規格');
+             // $table->string('groupe')->nullable()->comment('グループ');
+             // $table->string('hidden_price')->nullable()->comment('価格非表示');
+             // $table->boolean('zaikokanri')->nullable()->comment('在庫管理');
+             // $table->string('zaikosuu')->nullable()->comment('在庫数');
+             // $table->string('uwagaki_item_name')->nullable()->comment('上書き商品名');
+             // $table->string('uwagaki_kikaku')->nullable()->comment('上書き規格');
          });
      }
 
@@ -31,12 +31,12 @@ class AddBuyerRecommendsTable extends Migration
      public function down()
      {
          Schema::table('buyer_recommends', function (Blueprint $table) {
-             $table->dropColumn('groupe');
-             $table->dropColumn('hidden_price');
-             $table->dropColumn('zaikokanri');
-             $table->dropColumn('zaikosuu');
-             $table->dropColumn('uwagaki_item_name');
-             $table->dropColumn('uwagaki_kikaku');
+             // $table->dropColumn('groupe');
+             // $table->dropColumn('hidden_price');
+             // $table->dropColumn('zaikokanri');
+             // $table->dropColumn('zaikosuu');
+             // $table->dropColumn('uwagaki_item_name');
+             // $table->dropColumn('uwagaki_kikaku');
          });
      }
 }
