a8c47235b0e694b0fd91ad02dae2089d591119ef
arcion-git <info@arcion.jp>
2023-06-18 16:24:39 +0900

    s

--- app/Http/Controllers/LoginPageController.php
+++ app/Http/Controllers/LoginPageController.php
@@ -2322,12 +2322,8 @@ class LoginPageController extends Controller
           $special_price_item = SpecialPrice::where(['item_id'=>$item->item_id,'sku_code'=>$item->sku_code,'price_groupe'=>$price_groupe->price_groupe])
           // ->whereDate('start', '>=' , $now)
           ->whereDate('end', '<=', $now)->first();
-
-          $iteration++;
-          if ($iteration === 2) {
-              dd($special_price_item);
-          }
           if(isset($special_price_item)){
+            dd($special_price_item)
             $message = $item->item_name.'は掲載期限を過ぎているので注文できません。';
             $data=[
               'message' => $message,
