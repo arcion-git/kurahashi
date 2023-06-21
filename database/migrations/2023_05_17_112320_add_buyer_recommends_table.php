b019b6b7e8d1e1eb025ead7ecdf71093a70e5fae
arcion-git <info@arcion.jp>
2023-06-21 23:07:14 +0900

    s

--- database/migrations/2023_05_17_112320_add_buyer_recommends_table.php
+++ database/migrations/2023_05_17_112320_add_buyer_recommends_table.php
@@ -13,15 +13,15 @@ class AddBuyerRecommendsTable extends Migration
      */
      public function up()
      {
-         // Schema::table('buyer_recommends', function (Blueprint $table) {
-         //     $table->string('groupe')->nullable()->comment('グループ');
-         //     $table->string('hidden_price')->nullable()->comment('価格非表示');
-         //     $table->boolean('zaikokanri')->nullable()->comment('在庫管理');
-         //     $table->string('zaikosuu')->nullable()->comment('在庫数');
-         //     $table->string('uwagaki_item_name')->nullable()->comment('上書き商品名');
-         //     $table->string('uwagaki_kikaku')->nullable()->comment('上書き規格');
-         //     $table->string('gentei_store')->nullable()->comment('限定店舗');
-         // });
+         Schema::table('buyer_recommends', function (Blueprint $table) {
+             $table->string('groupe')->nullable()->comment('グループ');
+             $table->string('hidden_price')->nullable()->comment('価格非表示');
+             $table->boolean('zaikokanri')->nullable()->comment('在庫管理');
+             $table->string('zaikosuu')->nullable()->comment('在庫数');
+             $table->string('uwagaki_item_name')->nullable()->comment('上書き商品名');
+             $table->string('uwagaki_kikaku')->nullable()->comment('上書き規格');
+             $table->string('gentei_store')->nullable()->comment('限定店舗');
+         });
 
          // インデックスを作成
          Schema::table('buyer_recommends', function (Blueprint $table) {
@@ -42,14 +42,14 @@ class AddBuyerRecommendsTable extends Migration
       */
      public function down()
      {
-         // Schema::table('buyer_recommends', function (Blueprint $table) {
-         //     $table->dropColumn('groupe');
-         //     $table->dropColumn('hidden_price');
-         //     $table->dropColumn('zaikokanri');
-         //     $table->dropColumn('zaikosuu');
-         //     $table->dropColumn('uwagaki_item_name');
-         //     $table->dropColumn('uwagaki_kikaku');
-         //     $table->dropColumn('gentei_store');
-         // });
+         Schema::table('buyer_recommends', function (Blueprint $table) {
+             $table->dropColumn('groupe');
+             $table->dropColumn('hidden_price');
+             $table->dropColumn('zaikokanri');
+             $table->dropColumn('zaikosuu');
+             $table->dropColumn('uwagaki_item_name');
+             $table->dropColumn('uwagaki_kikaku');
+             $table->dropColumn('gentei_store');
+         });
      }
 }
