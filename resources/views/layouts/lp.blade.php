a28c84ed6147a98562e7df49db73aa583b3e5478
arcion-git <info@arcion.jp>
2023-11-19 00:12:34 +0900

    s

--- resources/views/layouts/lp.blade.php
+++ resources/views/layouts/lp.blade.php
@@ -106,7 +106,7 @@
           <div class="flex-container external_link">
             <ul class="">
               <!-- <li><a href="https://www.kurahashi.co.jp/" target="_blank">株式会社クラハシオフィシャルサイト<img src="https://setonagi.net/wp-content/themes/welcart_basic-beldad-expo/assets/images/expo/footer_link.png"></a></li> -->
-              <li><a href="http://u-midas.com/" target="_blank">株式会U-midasオフィシャルサイト<img src="https://setonagi.net/wp-content/themes/welcart_basic-beldad-expo/assets/images/expo/footer_link.png"></a></li>
+              <li><a href="http://u-midas.com/" target="_blank">株式会社U-midasオフィシャルサイト<img src="https://setonagi.net/wp-content/themes/welcart_basic-beldad-expo/assets/images/expo/footer_link.png"></a></li>
             </ul>
           </div>
           <div class="flex-container sns_link">
