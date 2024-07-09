7527c0d245bf19a1e2023eeeffcb35ac1a4f83e8
arcion-git <info@arcion.jp>
2024-07-09 10:35:33 +0900

    s

--- resources/views/admin/auth/setonagiuser.blade.php
+++ resources/views/admin/auth/setonagiuser.blade.php
@@ -75,47 +75,6 @@
                     @endif
                   </td>
                   <td class="text-center">
-                      @if($user->setonagi && $user->setonagi->kakebarai_sinsa == 'ご利用可' && $user->setonagi->kakebarai_riyou == '')
-                      <form action="{{ url('/admin/riyoukyoka') }}" method="POST" class="form-horizontal">
-                          {{ csrf_field() }}
-                          <input type="hidden" name="user_id" value="{{ $user->id }}" />
-                          <button type="submit" class="riyoukyoka_btn btn btn-warning">利用許可</button>
-                      </form>
-                      @endif
-                      @if($user->setonagi && $user->setonagi->kakebarai_sinsa == 'ご利用不可' && $user->setonagi->setonagi_ok == '')
-                      <form action="{{ url('/admin/card_riyoukyoka') }}" method="POST" class="form-horizontal">
-                          {{ csrf_field() }}
-                          <input type="hidden" name="user_id" value="{{ $user->id }}" />
-                          <button type="submit" class="card_riyoukyoka_btn btn btn-primary">カード払いのみで利用許可</button>
-                      </form>
-                      @endif
-                      @if($user->setonagi && ($user->setonagi->kakebarai_riyou == '1' || $user->setonagi->setonagi_ok == '1'))
-                      <form action="{{ url('/admin/riyouteisi') }}" method="POST" class="form-horizontal">
-                          {{ csrf_field() }}
-                          <input type="hidden" name="user_id" value="{{ $user->id }}" />
-                          <button type="submit" class="riyouteisi_btn btn btn-success">利用停止</button>
-                      </form>
-                      @endif
-                  </td>
-                  <td class="text-center">
-                      @if($user->setonagi)
-                          @if($user->setonagi->kakebarai_riyou == '1')
-                          利用許可済
-                          @elseif($user->setonagi->setonagi_ok == '1')
-                          カード払い利用許可済
-                          @elseif($user->setonagi->kakebarai_sinsa == 'ご利用可' && $user->setonagi->kakebarai_riyou == '0')
-                          利用許可待ち
-                          @elseif($user->setonagi->kakebarai_sinsa == '審査受付中')
-                          審査中
-                          @else
-                          利用不可
-                          @endif
-                      @else
-                      利用不可
-                      @endif
-                  </td>
-
-                  <!-- <td class="text-center">
                       @if($user->setonagi()->kakebarai_sinsa == 'ご利用可' & $user->setonagi()->kakebarai_riyou == '')
                       <form action="{{ url('/admin/riyoukyoka') }}" method="POST" class="form-horizontal">
                         {{ csrf_field() }}
@@ -150,7 +109,7 @@
                       @else
                       利用不可
                       @endif
-                  </td> -->
+                  </td>
                 </tr>
                 @endforeach
 
