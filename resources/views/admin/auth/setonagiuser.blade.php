83f0b5638c07ef75c812305d926967e82e9b4175
arcion-git <info@arcion.jp>
2024-07-09 11:33:53 +0900

    s

--- resources/views/admin/auth/setonagiuser.blade.php
+++ resources/views/admin/auth/setonagiuser.blade.php
@@ -102,17 +102,7 @@
                     @endif
                   </td>
                   <td class="text-center">
-                      <!-- @if($user->setonagi()->kakebarai_riyou == 1)
-                      利用許可済
-                      @elseif($user->setonagi()->setonagi_ok == 1)
-                      カード払い利用許可済
-                      @elseif($user->setonagi()->kakebarai_sinsa == 'ご利用可' & $user->setonagi()->kakebarai_riyou == 0)
-                      利用許可待ち
-                      @elseif($user->setonagi()->kakebarai_sinsa == '審査受付中')
-                      審査中
-                      @else
-                      利用不可
-                      @endif -->
+
                   </td>
                 </tr>
                 @endforeach
