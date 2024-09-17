fef69d05ad801c1fe318a1ed67ef4179794d386e
arcion-git <info@arcion.jp>
2024-09-17 16:55:54 +0900

    s

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -666,7 +666,7 @@ class AdminPageController extends Controller
         'form_params' => [
           'traderCode' => $kakebarai_traderCode,
           // バイヤーid
-          'buyerId' => $user_id,
+          'buyerId' => $user_id.$envi,
           'buyerTelNo' => '',
           'passWord' => $kakebarai_passWord
         ]
